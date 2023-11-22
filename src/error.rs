6db3e5d47f82a1ee869f0b9a3d0537996c7dfafd
Adrian John <adrianjohn6543@gmail.com>
2023-11-22 21:53:25 +0100

    add some util methods and a Debug impl for error mod

--- src/error.rs
+++ src/error.rs
@@ -1,2 +1,21 @@
+use std::fmt::{Debug, Formatter};
+
 pub struct ParseError(());
 
+impl ParseError {
+    pub const fn new() -> Self {
+        Self(())
+    }
+}
+
+impl From<()> for ParseError {
+    fn from(_value: ()) -> Self {
+        ParseError(())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, _f: &mut Formatter<'_>) -> std::fmt::Result {
+        Ok(())
+    }
+}
