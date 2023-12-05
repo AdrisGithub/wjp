5a9b9ae13788a148d2b867cfe29fc8f18f1e0df2
Adrian John <adrianjohn6543@gmail.com>
2023-12-05 20:34:08 +0100

    add more default impl to make a Value from several types

--- src/serializer.rs
+++ src/serializer.rs
@@ -1,4 +1,6 @@
 use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet};
+use std::isize;
+use std::str::FromStr;
 
 use crate::error::ParseError;
 use crate::values::Values;
@@ -192,5 +194,139 @@ impl<T: TryFrom<Values>> TryFrom<Values> for Vec<T> {
             post.push(T::try_from(pre.pop().unwrap()).map_err(|_err| ParseError::new())?)
         }
         Ok(post)
+    }
+}
+
+impl TryFrom<Values> for char {
+    type Error = ParseError;
+    fn try_from(value: Values) -> Result<Self, Self::Error> {
+        String::try_from(value)?
+            .chars().next()
+            .ok_or(ParseError::new())
+    }
+}
+
+impl TryFrom<Values> for String {
+    type Error = ParseError;
+    fn try_from(value: Values) -> Result<Self, Self::Error> {
+        value.get_string().ok_or(ParseError::new())
+    }
+}
+
+impl TryFrom<Values> for usize {
+    type Error = ParseError;
+    fn try_from(value: Values) -> Result<Self, Self::Error> {
+        value.get_number()
+            .map(|f| f.to_string())
+            .map(|s| usize::from_str(s.as_str()))
+            .ok_or(ParseError::new())?
+            .map_err(|_err| ParseError::new())
+    }
+}
+
+impl TryFrom<Values> for u8 {
+    type Error = ParseError;
+    fn try_from(value: Values) -> Result<Self, Self::Error> {
+        usize::try_from(value)
+            .map(u8::try_from)?
+            .map_err(|_err| ParseError::new())
+    }
+}
+
+impl TryFrom<Values> for u16 {
+    type Error = ParseError;
+    fn try_from(value: Values) -> Result<Self, Self::Error> {
+        usize::try_from(value)
+            .map(u16::try_from)?
+            .map_err(|_err| ParseError::new())
+    }
+}
+
+impl TryFrom<Values> for u32 {
+    type Error = ParseError;
+    fn try_from(value: Values) -> Result<Self, Self::Error> {
+        usize::try_from(value)
+            .map(u32::try_from)?
+            .map_err(|_err| ParseError::new())
+    }
+}
+
+impl TryFrom<Values> for u64 {
+    type Error = ParseError;
+    fn try_from(value: Values) -> Result<Self, Self::Error> {
+        usize::try_from(value)
+            .map(u64::try_from)?
+            .map_err(|_err| ParseError::new())
+    }
+}
+
+impl TryFrom<Values> for u128 {
+    type Error = ParseError;
+    fn try_from(value: Values) -> Result<Self, Self::Error> {
+        usize::try_from(value)
+            .map(u128::try_from)?
+            .map_err(|_err| ParseError::new())
+    }
+}
+
+impl TryFrom<Values> for isize {
+    type Error = ParseError;
+    fn try_from(value: Values) -> Result<Self, Self::Error> {
+        String::try_from(value)
+            .map(|str| isize::from_str(str.as_str()))?
+            .map_err(|_err| ParseError::new())
+    }
+}
+
+impl TryFrom<Values> for i8 {
+    type Error = ParseError;
+    fn try_from(value: Values) -> Result<Self, Self::Error> {
+        isize::try_from(value)
+            .map(i8::try_from)?
+            .map_err(|_err| ParseError::new())
+    }
+}
+
+impl TryFrom<Values> for i16 {
+    type Error = ParseError;
+    fn try_from(value: Values) -> Result<Self, Self::Error> {
+        isize::try_from(value)
+            .map(i16::try_from)?
+            .map_err(|_err| ParseError::new())
+    }
+}
+
+impl TryFrom<Values> for i32 {
+    type Error = ParseError;
+    fn try_from(value: Values) -> Result<Self, Self::Error> {
+        isize::try_from(value)
+            .map(i32::try_from)?
+            .map_err(|_err| ParseError::new())
+    }
+}
+
+impl TryFrom<Values> for i64 {
+    type Error = ParseError;
+    fn try_from(value: Values) -> Result<Self, Self::Error> {
+        isize::try_from(value)
+            .map(i64::try_from)?
+            .map_err(|_err| ParseError::new())
+    }
+}
+
+impl TryFrom<Values> for i128 {
+    type Error = ParseError;
+    fn try_from(value: Values) -> Result<Self, Self::Error> {
+        isize::try_from(value)
+            .map(i128::try_from)?
+            .map_err(|_err| ParseError::new())
+    }
+}
+
+impl TryFrom<Values> for bool {
+    type Error = ParseError;
+    fn try_from(value: Values) -> Result<Self, Self::Error> {
+        value.get_bool()
+            .ok_or(ParseError::new())
     }
 }
