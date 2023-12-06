bb9e693bb87e20436c516a38d7bb8416e873ab7e
Adrian John <adrianjohn6543@gmail.com>
2023-12-06 23:01:26 +0100

    add a test for the map macro

--- src/macros.rs
+++ src/macros.rs
@@ -1,8 +1,11 @@
 #[macro_export]
 macro_rules! map (
-    {$(($key:expr,$value:expr)), + } => {
+    () => {
+      std::collections::HashMap::new()
+    };
+    ($(($key:expr,$value:expr)), + ) => {
         {
-            let mut m = std::collections::HashMap::new();
+            let mut m = std::collections::HashMap::with_capacity(3);
             $(
                 m.insert(String::from($key), $value);
              )+
@@ -10,3 +13,20 @@ macro_rules! map (
         }
     };
 );
+
+
+#[cfg(test)]
+mod tests{
+    use std::collections::HashMap;
+    #[test]
+    pub fn with_empty_params(){
+        assert_eq!(map!(),HashMap::<&str,&str>::new())
+    }
+    #[test]
+    pub fn with_filled_params(){
+        let mut map = HashMap::new();
+        map.insert(String::from("test"),123);
+        assert_eq!(map!(("test",123)),map)
+    }
+
+}
