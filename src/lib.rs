32ad26d688660400268ebdbe5cd40fd148b42a8f
Adrian John <adrianjohn6543@gmail.com>
2023-12-09 18:57:19 +0100

    improve the exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
-pub mod deserialize;
-pub mod error;
-pub mod helper;
-pub mod macros;
-pub mod parser;
-pub mod serializer;
+pub use deserialize::Deserialize;
+pub use error::ParseError;
+pub use helper::SerializeHelper;
+pub use serializer::Serialize;
+pub use values::Values;
+
+mod deserialize;
+mod error;
+mod helper;
+mod macros;
+mod parser;
+mod serializer;
 #[cfg(test)]
 mod test;
-pub mod values;
+mod values;
